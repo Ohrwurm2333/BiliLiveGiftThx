818f4b128333923425e70d5c415e6cda8e96933c
lc4t <lc4t0.0@gmail.com>
2020-02-05 03:56:48 +0800

    fix

--- danmu/bili_danmu_giftthx.py
+++ danmu/bili_danmu_giftthx.py
@@ -30,49 +30,49 @@ class DanmuGiftThx(WsDanmuClient):
                 cache_gift = []
                 for i in range(qlength):
                     cache_gift.append(self.GIFT_QUEUE.get())
-                # print(cache_gift)
-                # cache_gift是所有没处理的送礼物的信息
-                # 现在将他们合并为一个list
-                for gift_info in cache_gift:
-                    if gift_info.get('room') != roomid:
-                        print('error room id')
-                        exit(0)
-                    username, gift_name, gift_num, t = gift_info.get('username'), gift_info.get(
-                        'gift_name'), gift_info.get('gift_num'), gift_info.get('t')
-                    if username not in wait_to_send_danmu:
-                        wait_to_send_danmu[username] = {}    # 新建username
-                    if gift_name not in wait_to_send_danmu.get(username):
-                        wait_to_send_danmu[username].update(
-                            {gift_name: {'gift_num': gift_num, 't': t}})   # username->gift_name
-                    else:
-                        # 查找已经送了的有多少
-                        already_num = wait_to_send_danmu[username].get(
-                            gift_name, {}).get('gift_num', 0)  # 已经送了的
-                        wait_to_send_danmu[username][gift_name].update(
-                            {'gift_num': gift_num + already_num, 't': t})  # 更新数量
+            # print(cache_gift)
+            # cache_gift是所有没处理的送礼物的信息
+            # 现在将他们合并为一个list
+            for gift_info in cache_gift:
+                if gift_info.get('room') != roomid:
+                    print('error room id')
+                    exit(0)
+                username, gift_name, gift_num, t = gift_info.get('username'), gift_info.get(
+                    'gift_name'), gift_info.get('gift_num'), gift_info.get('t')
+                if username not in wait_to_send_danmu:
+                    wait_to_send_danmu[username] = {}    # 新建username
+                if gift_name not in wait_to_send_danmu.get(username):
+                    wait_to_send_danmu[username].update(
+                        {gift_name: {'gift_num': gift_num, 't': t}})   # username->gift_name
+                else:
+                    # 查找已经送了的有多少
+                    already_num = wait_to_send_danmu[username].get(
+                        gift_name, {}).get('gift_num', 0)  # 已经送了的
+                    wait_to_send_danmu[username][gift_name].update(
+                        {'gift_num': gift_num + already_num, 't': t})  # 更新数量
 
-                # print(wait_to_send_danmu)
+            # print(wait_to_send_danmu)
 
-                # 检查时间是否达到推出标准
-                # 这里可以重写感谢弹幕
-                for username, gifts in wait_to_send_danmu.items():
-                    for gift_name, info in gifts.items():
-                        gift_num = info.get('gift_num')
-                        if gift_num == 0:
-                            continue
-                        if time.time() - info.get('t') > DanmuGiftThx.DELAY_SECOND:
-                            await self.send_danmu(DanmuGiftThx.GIFT_MSG.format(username=username, num=gift_num, giftname=gift_name))
-                            wait_to_send_danmu[username][gift_name].update({'gift_num': 0})
-                await asyncio.sleep(1)
+            # 检查时间是否达到推出标准
+            # 这里可以重写感谢弹幕
+            for username, gifts in wait_to_send_danmu.items():
+                for gift_name, info in gifts.items():
+                    gift_num = info.get('gift_num')
+                    if gift_num == 0:
+                        continue
+                    if time.time() - info.get('t') > DanmuGiftThx.DELAY_SECOND:
+                        await self.send_danmu(DanmuGiftThx.GIFT_MSG.format(username=username, num=gift_num, giftname=gift_name))
+                        wait_to_send_danmu[username][gift_name].update({'gift_num': 0})
+            await asyncio.sleep(1)
 
     async def send_danmu(self, text, default_length=30):
+        # print('try:', text, len(text))
         msg = text[0:default_length]
         json_rsp = await self.user.req_s(UtilsReq.send_danmu, self.user, msg, self._room_id)
-        raw_json = json.dumps(json_rsp, ensure_ascii=False)
-        print(raw_json)
-        if raw_json.get('msg', '') == 'msg in 1s':
-            await asyncio.sleep(1)
-            return self.send_danmu(text, default_length)
+        print(json_rsp)
+        if json_rsp.get('msg', '') == 'msg in 1s':  # msg repeat 不处理了
+            await asyncio.sleep(0.5)
+            return await self.send_danmu(text, default_length)
         if len(text) > default_length:
             await asyncio.sleep(1)
             await self.send_danmu(text[default_length:], default_length)
